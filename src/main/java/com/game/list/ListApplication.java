bda1482e7a9b1faf976540641553d82291bf3ae6
julio <julio.prog124@gmail.com>
2023-11-15 13:02:56 -0300

    feat: properties,game, orm created

--- src/main/java/com/game/list/ListApplication.java
+++ src/main/java/com/game/list/ListApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListApplication.class, args);
+
+
+
 	}
 
 }
---
+++ src/main/java/com/game/list/domain/entities/Game.java
@@ -0,0 +1,30 @@
+package com.game.list.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private Double score;
+    private String imgUrl;
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+
+}
