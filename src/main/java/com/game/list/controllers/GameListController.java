68b84f325cb06a5a411df2ccf4ecff68b1fbd6f5
julio <julio.prog124@gmail.com>
2023-11-15 16:56:53 -0300

    feat: Get games by list

--- src/main/java/com/game/list/controllers/GameListController.java
+++ src/main/java/com/game/list/controllers/GameListController.java
@@ -1,9 +1,12 @@
 package com.game.list.controllers;
 
 import com.game.list.domain.records.GameListDTO;
+import com.game.list.domain.records.GameMinDTO;
 import com.game.list.services.GameListService;
+import com.game.list.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +19,16 @@ public class GameListController {
     @Autowired
     GameListService gameListService;
 
+    @Autowired
+    GameService gameService;
+
     @GetMapping
     public List<GameListDTO>    getAllGameList(){
         return gameListService.getAllGameList();
+    }
+
+    @GetMapping(value = "/{id}/games")
+    public List<GameMinDTO> getAllGamesByList(@PathVariable Long id){
+        return gameService.getAllGamesMinByList(id);
     }
 }
--- src/main/java/com/game/list/services/GameService.java
+++ src/main/java/com/game/list/services/GameService.java
@@ -3,6 +3,7 @@ package com.game.list.services;
 import com.game.list.domain.entities.Game;
 import com.game.list.domain.records.GameDTO;
 import com.game.list.domain.records.GameMinDTO;
+import com.game.list.projections.GameMinProjection;
 import com.game.list.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +29,14 @@ public class GameService {
                 new GameMinDTO(x.getId(),x.getTitle(),x.getYear(),x.getImgUrl(),x.getShortDescription()))
                 .toList();
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> getAllGamesMinByList(Long listId){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        return list.stream().map( x ->
+                        new GameMinDTO(x.getId(),x.getTitle(),x.getYear(),x.getImgUrl(),x.getShortDescription()))
+                .toList();
+    }
+
 
 }
