f7f73f378290bfc35e143acb2b5566c5197bf7fe
julio <julio.prog124@gmail.com>
2023-11-15 13:28:21 -0300

    feat: Game seed

--- src/main/java/com/game/list/domain/entities/Game.java
+++ src/main/java/com/game/list/domain/entities/Game.java
@@ -19,7 +19,7 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
     @Column(columnDefinition = "TEXT")
